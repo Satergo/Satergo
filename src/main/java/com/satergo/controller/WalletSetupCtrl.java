a3112c1d4b81f48ef9d3b670437f5b822fc7e6cb
satsen <95986745+satsen@users.noreply.github.com>
2022-01-10 09:55:07 +0100

    Added node configuration information to wallet setup page

--- src/main/java/com/satergo/controller/WalletSetupCtrl.java
+++ src/main/java/com/satergo/controller/WalletSetupCtrl.java
@@ -1,21 +1,37 @@
 package com.satergo.controller;
 
-import com.satergo.Load;
-import com.satergo.Main;
-import com.satergo.Utils;
-import com.satergo.Wallet;
+import com.satergo.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.FileChooser;
 
 import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class WalletSetupCtrl implements SetupPage.WithLanguage, SetupPage.CustomLeft {
 
+	@FXML private Label nodeConfigurationInfo;
 	@FXML private Parent root;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		String langProperty;
+		String param2;
+		if (Main.programData().blockchainNodeKind.get() == ProgramData.BlockchainNodeKind.EMBEDDED_FULL_NODE) {
+			langProperty = "nodeConfigurationEmbedded_path";
+			param2 = Main.node.nodeDirectory.getAbsolutePath();
+		} else {
+			langProperty = "nodeConfigurationRemote_address";
+			param2 = Main.programData().nodeAddress.get();
+		}
+		nodeConfigurationInfo.setText(Main.lang(langProperty).formatted(Main.programData().nodeNetworkType.get().toString(), param2));
+	}
 
 	@FXML
 	public void createWallet(MouseEvent e) {
