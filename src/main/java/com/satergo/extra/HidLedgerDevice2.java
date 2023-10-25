a27ba91c261fa856ea9a14d9c3a6f19bdf95feca
satsen <95986745+satsen@users.noreply.github.com>
2023-10-25 09:24:48 +0100

    Ledger

--- src/main/java/com/satergo/extra/HidLedgerDevice2.java
+++ src/main/java/com/satergo/extra/HidLedgerDevice2.java
@@ -1,5 +1,7 @@
 package com.satergo.extra;
 
+import com.satergo.jledger.APDUCommand;
+import com.satergo.jledger.APDUResponse;
 import com.satergo.jledger.LedgerDevice;
 import org.hid4java.HidDevice;
 
@@ -7,12 +9,14 @@ import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class HidLedgerDevice2 implements LedgerDevice {
 
-	private final int channel = (int) Math.floor(Math.random() * 0xffff);
-	private static final int packetSize = 64;
+	private static final int PACKET_SIZE = 64;
 	private static final byte TAG = 0x05;
+
+	private final int channel = (int) Math.floor(Math.random() * 0xffff);
 
 	private final HidDevice hidDevice;
 
@@ -74,27 +78,20 @@ public class HidLedgerDevice2 implements LedgerDevice {
 			public static final ResponseAcc INITIAL = new ResponseAcc(new byte[0], 0, 0);
 		}
 		public ResponseAcc reduceResponse(ResponseAcc acc, ByteBuffer chunk) {
-			System.out.println(chunk + " = " + Arrays.toString(chunk.array()));
 			ResponseAcc temp = acc == null ? ResponseAcc.INITIAL : acc;
 			byte[] data = temp.data;
 			int dataLength = temp.dataLength,
 					sequence = temp.sequence;
 
-			//
-			//
-			if (Short.toUnsignedInt(chunk.getShort()) != channel) {
+			if (Short.toUnsignedInt(chunk.getShort()) != channel)
 				throw new RuntimeException("Invalid channel");
-			}
 
-			if (chunk.get() != TAG) {
+			if (chunk.get() != TAG)
 				throw new RuntimeException("Invalid tag");
-			}
 
-			if (Short.toUnsignedInt(chunk.getShort()) != sequence) {
+			if (Short.toUnsignedInt(chunk.getShort()) != sequence)
 				throw new RuntimeException("Invalid sequence");
-			}
 
-			System.out.println("pos: " + chunk.position() + ", capacity: " + chunk.capacity());
 			if (acc == null) {
 				dataLength = Short.toUnsignedInt(chunk.getShort());
 			}
@@ -121,27 +118,77 @@ public class HidLedgerDevice2 implements LedgerDevice {
 
 	@Override
 	public int write(byte[] bytes) {
-		List<byte[]> blocks = new HidFraming(channel, packetSize).makeBlocks(bytes);
+		List<byte[]> blocks = new HidFraming(channel, PACKET_SIZE).makeBlocks(bytes);
 		for (byte[] block : blocks) {
 			hidDevice.write(block, block.length, (byte) 0);
 		}
-		return hidDevice.write(bytes, bytes.length, (byte) 0);
+		return 0;
+	}
+
+	/** Reads a packet with a maximum size of 64 bytes */
+	private HidFraming.ResponseAcc readSinglePacket() {
+		HidFraming framing = new HidFraming(channel, PACKET_SIZE);
+		HidFraming.ResponseAcc acc = null;
+		while (framing.getReducedResult(acc) == null) {
+			byte[] buffer = new byte[PACKET_SIZE];
+			int bytesRead = hidDevice.read(buffer);
+			if (bytesRead == 0) break;
+			acc = framing.reduceResponse(acc, ByteBuffer.wrap(buffer));
+		}
+		return acc;
+	}
+
+	/** Reads all available packets into one response */
+	private byte[] readResponse() {
+		ArrayList<HidFraming.ResponseAcc> packets = new ArrayList<>();
+		int length = 0;
+		while (true) {
+			HidFraming.ResponseAcc responseAcc = readSinglePacket();
+			if (responseAcc == null)
+				break;
+			length += responseAcc.dataLength;
+			packets.add(responseAcc);
+		}
+		byte[] full = new byte[length];
+		for (HidFraming.ResponseAcc packet : packets) {
+			System.arraycopy(packet.data, 0, full, (packet.sequence - 1) * PACKET_SIZE, packet.dataLength);
+		}
+		return full;
 	}
 
 	@Override
 	public int read(byte[] bytes) {
-		HidFraming framing = new HidFraming(channel, packetSize);
-		byte[] result;
-		HidFraming.ResponseAcc acc = null;
-		int read = -1;
-		while ((result = framing.getReducedResult(acc)) == null) {
-			byte[] buffer = new byte[bytes.length];
-			read = hidDevice.read(buffer);
-			System.out.println("read (count " + read + ") = " + toStringUnsigned(buffer));
-			acc = framing.reduceResponse(acc, ByteBuffer.wrap(buffer));
+		throw new UnsupportedOperationException();
+//		HidFraming framing = new HidFraming(channel, PACKET_SIZE);
+//		byte[] result;
+//		HidFraming.ResponseAcc acc = null;
+//		int read = -1;
+//		while ((result = framing.getReducedResult(acc)) == null) {
+//			byte[] buffer = new byte[bytes.length];
+//			read = hidDevice.read(buffer);
+//			System.out.println("read (count " + read + ") = " + toStringUnsigned(buffer));
+//			acc = framing.reduceResponse(acc, ByteBuffer.wrap(buffer));
+//		}
+//		System.arraycopy(result, 0, bytes, 0, result.length);
+//		return read;
+	}
+
+	@Override
+	public APDUResponse readAPDU(int dataSize) {
+		return new APDUResponse(readResponse());
+	}
+
+	private final ReentrantLock lock = new ReentrantLock();
+
+	@Override
+	public APDUResponse exchange(APDUCommand apdu, int responseSize) {
+		lock.lock();
+		try {
+			writeAPDU(apdu);
+			return readAPDU(responseSize);
+		} finally {
+			lock.unlock();
 		}
-		System.arraycopy(result, 0, bytes, 0, result.length);
-		return read;
 	}
 
 	private static String toStringUnsigned(byte[] bytes) {
