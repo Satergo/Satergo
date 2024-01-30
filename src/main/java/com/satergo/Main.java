ef0c9a7e28fe6c1675df130950269779e6b120a2
satsen <95986745+satsen@users.noreply.github.com>
2024-01-30 08:11:44 +0000

    Version 1.7.0

--- src/main/java/com/satergo/Main.java
+++ src/main/java/com/satergo/Main.java
@@ -29,8 +29,8 @@ import java.util.*;
 
 public class Main extends Application {
 
-	public static final String VERSION = "1.6.0";
-	public static final int VERSION_CODE = 8;
+	public static final String VERSION = "1.7.0";
+	public static final int VERSION_CODE = 9;
 
 	public static EmbeddedNode node;
 	// from command line
